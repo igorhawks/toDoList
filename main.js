3848088af866a3e0aaeda67966b0d1cfd32b128b
igorhawks <125986266+igorhawks@users.noreply.github.com>
2024-05-01 23:07:02 +0000

    correcao

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const botaoAdiciona = document.querySelector("button")
 const inputTarefa = document.querySelector("#tarefa")
 const lista = document.querySelector("#lista")
-const feito = document.querySelector("#feito")
+
 
 //cria um listener para monitorar o clique do botão
 botaoAdiciona.addEventListener('click', adicionaTarefa)
@@ -13,28 +13,36 @@ function adicionaTarefa(){
    if(inputTarefa.value.length > 3){
         //criar um elemento <div>
         let itemDiv = document.createElement('div')
-        itemDiv.classList.add("concluido")
+        itemDiv.classList.add("divLista")
+
        //cria elemento <li>
        let li = document.createElement('li')
+
        //adicionar o texto ao li
        li.innerText = inputTarefa.value
+
        //adicionar a li a div
        itemDiv.appendChild(li)
+
        //criar um botao para feito
        let btFeito = document.createElement('button')
-       btFeito.classList.add("feito")
+       btFeito.id("feito")
+       //btFeito.innerHTML = '<i class="fa fa-check-circle fa-xs">V</i>'
+
        let btExcluir = document.createElement('button')
-       btFeito.innerHTML = "V"
-       btExcluir.innerHTML = "X"
-        //adiciona um icone de "check" ao botao
-        btFeito.classList.add("checked")
-        btExcluir.classList.add("excluir")
+       btExcluir.id("excluir")
+       //btExcluir.innerHTML = '<i class="fa fa-minus-circle fa-xs">X</i>'
+           
+
         //adiciona o botão à div
         itemDiv.appendChild(btFeito)
         itemDiv.appendChild(btExcluir)
+
+        //adicionar a <div> dentro da Lista
         lista.appendChild(itemDiv)
-       //limpa o conteúdo do input
-       inputTarefa.value = ''
+       
+        //limpa o conteúdo do input
+        inputTarefa.value = ''
    }
 
 }
@@ -48,13 +56,21 @@ inputTarefa.addEventListener("keyup", (evento) =>{
    })
 
 //monitora cliques em qualquer local da lista
-lista.addEventListener('click', marcaFeito)
+lista.addEventListener('click', concluirExcluir)
+
+//monitorar clique no botão concluido
+feito.addEventListener('click', marcaFeito)
 
 
 //verifica se o que foi clicado dentro da lista foi um botão
-function marcaFeito(evento){
-    if(evento.target.nodeName =="BUTTON")
+function concluirExcluir(evento){
+    if(evento.target.nodeName =="BUTTON" && evento.target.id == "feito")
     {
         evento.target.parentElement.classList.toggle('completa')
+    }
+    if(evento.target.nodeName == "BUTTON" && evento.target.id == "excluir"){
+        let paiDoBotaoRemover = evento.target.parentElement;
+        // Removemos o elemento pai do botão "Remover", que é o item a ser excluído
+        paiDoBotaoRemover.remove();
     }
 }
